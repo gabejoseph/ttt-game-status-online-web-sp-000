d5f66d1f128d97bdf76d31058e0f03edc1f19cc2
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-03-22 22:16:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,8 +44,7 @@ def over?(board)
 end
 
 def winner(board)
-  binding.pry
-  if draw?(board) == nil && won?(board) == "X"
+  if draw?(board) == false && won?(board) == "X"
     return "X"
   else draw?(board) == nil 
     return nil
